1f47d0e069cec0165925698c8977b43dd37957c8
Andre Witte <andre.witte@freenet.de>
2026-03-11 11:25:03 +0100

    Release v1.4.0

--- dev.sh
+++ dev.sh
@@ -6,7 +6,7 @@
 #
 # Description: dDEV symfony dev shortening tools
 # Usage: dev [OPTIONS]
-# Version: 1.3.4
+# Version: 1.4.0
 # https://github.com/akaw/dev/
 
 # Helper function: Reads the latest version number from Git tags
